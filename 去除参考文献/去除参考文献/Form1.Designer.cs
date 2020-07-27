46baf4f673263bdc7045d80bddbade1497f46777
zzzzz_yuan <57674496+czy960323@users.noreply.github.com>
2020-07-27 16:53:14 +0800

    Add files via upload

---
+++ 去除参考文献/去除参考文献/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace 去除参考文献
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearStart = new System.Windows.Forms.Button();
+            this.orgText = new System.Windows.Forms.TextBox();
+            this.targetText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // clearStart
+            // 
+            this.clearStart.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clearStart.Location = new System.Drawing.Point(509, 632);
+            this.clearStart.Name = "clearStart";
+            this.clearStart.Size = new System.Drawing.Size(89, 39);
+            this.clearStart.TabIndex = 0;
+            this.clearStart.Text = "clear";
+            this.clearStart.UseVisualStyleBackColor = true;
+            this.clearStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // orgText
+            // 
+            this.orgText.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.orgText.Location = new System.Drawing.Point(12, 12);
+            this.orgText.Multiline = true;
+            this.orgText.Name = "orgText";
+            this.orgText.Size = new System.Drawing.Size(1067, 317);
+            this.orgText.TabIndex = 1;
+            // 
+            // targetText
+            // 
+            this.targetText.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.targetText.Location = new System.Drawing.Point(12, 335);
+            this.targetText.Multiline = true;
+            this.targetText.Name = "targetText";
+            this.targetText.Size = new System.Drawing.Size(1067, 291);
+            this.targetText.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 689);
+            this.Controls.Add(this.targetText);
+            this.Controls.Add(this.orgText);
+            this.Controls.Add(this.clearStart);
+            this.Name = "Form1";
+            this.Text = "去除参考文献";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button clearStart;
+        private System.Windows.Forms.TextBox orgText;
+        private System.Windows.Forms.TextBox targetText;
+    }
+}
+
---
+++ 去除参考文献/去除参考文献/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 去除参考文献
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String orgText = this.orgText.Text;
+            while (true)
+            {
+                int len = orgText.Length;
+
+                int start = 0, end = 0;
+                int i = 0;
+                for (i = 0; i < len; i++)
+                {
+                    if (orgText.Substring(i,1) == "[")
+                        start = i;
+                    if (orgText.Substring(i, 1) == "]")
+                    {
+                        end = i;
+                        orgText = orgText.Remove(start-1, end - start + 2);
+                        break;
+                    }
+                }
+
+                len = orgText.Length;
+                start = 0;
+                end = 0;
+                int del = 0;
+                int a = 0;
+                int j = 0;
+                for (j = 0; j < len-4; j++)
+                {
+                    if (orgText.Substring(j, 1) == "(")
+                        start = j;
+                    if (start != 0 && int.TryParse(orgText.Substring(j, 4), out a) == true)
+                        del = 1;
+                    if (orgText.Substring(j, 1) == ")" && del == 1)
+                    {
+                        end = j;
+                        orgText = orgText.Remove(start - 1, end - start + 2);
+                        break;
+                    }
+                }
+
+                if (i == len && j == len-4)
+                    break;
+            }
+            this.targetText.Text = orgText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
